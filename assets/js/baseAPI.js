d8c38a4d64ad7c44c068bd8804021488fdb59427
JGQ <2551216@qq.com>
2020-10-27 11:00:01 +0800

    注册和登录功能完成

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,14 @@
+//1.开发环境服务器地址
+var baseURL = "http://ajax.frontend.itheima.net"
+//1.测试环境服务器地址
+// var baseURL = "http://ajax.frontend.itheima.net"
+//1.生产环境服务器地址
+//var baseURL = "http://ajax.frontend.itheima.net"
+
+
+
+// 拦截所有ajax请求
+$.ajaxPrefilter(function (options) {
+    options.url = baseURL + options.url
+    // console.log(params.url);
+})
--- assets/js/login.js
+++ assets/js/login.js
@@ -29,11 +29,12 @@ $(function () {
   })
 
   //4.注册功能
+  var layer = layui.layer
   $('#form_reg').on('submit', function (e) {
     e.preventDefault();
     $.ajax({
       method: 'POST',
-      url: 'http://ajax.frontend.itheima.net/api/reguser',
+      url: '/api/reguser',
       data: {
         username: $('.reg-box [name=username]').val(),
         password: $('.reg-box [name=password]').val()
@@ -41,10 +42,34 @@ $(function () {
       success: function (res) {
         if (res.status != 0) {
           // 返回状态判断
-          return alert(res.message)
+          return layer.msg(res.message)
         }
         //提交成功后处理代码
-        alert(res.message)
+        layer.msg("注册成功,请登录")
+        // 自动切换到登录表单
+        $('#link_login').click()
+        // 重置form表单
+        $('#form_reg')[0].reset()
+      }
+    })
+  })
+
+  //5.登录功能
+  $('#form_login').submit(function (e) {
+    e.preventDefault()
+    $.ajax({
+      method: 'POST',
+      url: '/api/login',
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg(res.message)
+        }
+        layer.msg("恭喜您,登陆成功")
+        //保存token,未来的接口要用token
+        localStorage.setItem('token', res.token)
+        //跳转
+        // location.href = "/index.html"
       }
     })
   })
